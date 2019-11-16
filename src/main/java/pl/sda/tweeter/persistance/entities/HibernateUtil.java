49878229bfcaa60f9b7af58970e2760319345941
Mateusz Gliwka <gliwson@gmail.com>
2019-11-16 11:47:12 +0100

    update login

--- src/main/java/pl/sda/tweeter/persistance/entities/HibernateUtil.java
+++ src/main/java/pl/sda/tweeter/persistance/entities/HibernateUtil.java
@@ -2,29 +2,35 @@ package pl.sda.tweeter.persistance.entities;
 
 
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.service.ServiceRegistry;
+import org.hibernate.cfg.Configuration;
+
+import static pl.sda.tweeter.util.EnvironmentVariableUtil.getVariable;
 
 public class HibernateUtil {
 
     private static final SessionFactory sessionFactory = buildSessionFactory();
+    private static final String USERNAME = "hibernate.connection.username";
+    private static final String PASSWORD = "hibernate.connection.password";
+    private static final String URL = "hibernate.connection.url";
 
     private static SessionFactory buildSessionFactory() {
         try {
-            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .configure("hibernate.cfg.xml")
-                    .build();
-            Metadata metadata = new MetadataSources(serviceRegistry)
-                    .getMetadataBuilder()
-                    .build();
+//            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+//                    .configure("hibernate.cfg.xml")
+//                    .build();
+//            Metadata metadata = new MetadataSources(serviceRegistry)
+//                    .getMetadataBuilder()
+//                    .build();
 //            SessionFactory factory = new Configuration()
 //                    .configure("hibernate.cfg.xml")
 //                    .buildSessionFactory();
-            return metadata
-                    .getSessionFactoryBuilder()
-                    .build();
+
+            return new Configuration()
+                    .setProperty(USERNAME, getVariable(USERNAME))
+                    .setProperty(PASSWORD, getVariable(PASSWORD))
+                    .setProperty(URL, getVariable(URL) + getVariable(USERNAME))
+                    .configure()
+                    .buildSessionFactory();
         } catch (Throwable e) {
             System.err.println("Initial SessionFactory creation failed. " + e);
             throw new ExceptionInInitializerError();
@@ -38,4 +44,6 @@ public class HibernateUtil {
     public static void shutDown() {
         getSessionFactory().close();
     }
+
+
 }
--- src/main/java/pl/sda/tweeter/service/UserService.java
+++ src/main/java/pl/sda/tweeter/service/UserService.java
@@ -2,15 +2,24 @@ package pl.sda.tweeter.service;
 
 import pl.sda.tweeter.dao.UserDao;
 import pl.sda.tweeter.exception.IncorrectLoginOrPassword;
-import pl.sda.tweeter.persistance.entities.SecondMethodConnectionHibernate;
 import pl.sda.tweeter.persistance.entities.TbUser;
+
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
 
 public class UserService {
     private UserDao userDao = new UserDao();
-    private SecondMethodConnectionHibernate secondMethodConnectionHibernate = new SecondMethodConnectionHibernate();
 
     public TbUser getTbUser(String login, String password) throws IncorrectLoginOrPassword {
-        TbUser userByLogin = secondMethodConnectionHibernate.getUserByLogin(login);
+        TbUser userByLogin;
+
+        try {
+            userByLogin = userDao.getUserByLogin(login);
+        } catch (NoResultException e) {
+            throw new IncorrectLoginOrPassword("no user in database");
+        } catch (NonUniqueResultException e) {
+            throw new IncorrectLoginOrPassword("many user in database");
+        }
         if (userByLogin == null || !userByLogin.getPassword().equals(password)) {
             throw new IncorrectLoginOrPassword("incorrect login or password");
         }
