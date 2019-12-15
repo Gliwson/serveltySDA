705d1b18a1d26e9894d191715528af205477240b
Mateusz Gliwka <gliwson@gmail.com>
2019-12-15 14:05:09 +0100

    update login

--- twitter/src/main/java/tweeter/service/Logout.java
+++ twitter/src/main/java/tweeter/service/Logout.java
@@ -4,6 +4,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
 @WebServlet(urlPatterns = {"/logout"})
+@WebServlet(urlPatterns = {"/logout"})
+@WebServlet(urlPatterns = {"/logout"})
 public class Logout extends HttpServlet {
 
 
